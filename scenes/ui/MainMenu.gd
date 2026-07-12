2fe66a5fe1fcb5953dde8f042d17d4e109793ab8
Leonel <acostaleo18@gmail.com>
2026-07-12 14:47:18 -0600

    fix: corregir conexion boton SALIR en MainMenu

--- scenes/ui/MainMenu.gd
+++ scenes/ui/MainMenu.gd
@@ -11,12 +11,9 @@ func _on_btn_creditos_pressed():
 	await get_tree().create_timer(2.0).timeout
 	$Contenido/Botones/BtnCreditos.text = "CREDITOS"
 
-func _on_bnt_salir_pressed():
-	get_tree().quit()
-
 func _on_btn_tienda_pressed():
 	get_tree().change_scene_to_file("res://scenes/ui/UpgradeShop.tscn")
+	
 
-
-func _on_btn_salir_pressed() -> void:
-	pass # Replace with function body.
+func _on_btn_salir_pressed():
+	get_tree().quit()
