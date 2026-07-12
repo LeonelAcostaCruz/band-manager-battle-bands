087bd0bddc958b6b63eef87d3c5ce27e9c1a03bc
Leonel <acostaleo18@gmail.com>
2026-07-12 14:42:13 -0600

    fix: reconstruir MainMenu.tscn corrupto y restaurar Autoloads

--- scenes/ui/MainMenu.gd
+++ scenes/ui/MainMenu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready():
-	Input.mouse_mode=Input.MOUSE_MODE_VISIBLE
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _on_btn_jugar_pressed():
 	get_tree().change_scene_to_file("res://scenes/battle/BattleScene.tscn")
@@ -9,13 +9,14 @@ func _on_btn_jugar_pressed():
 func _on_btn_creditos_pressed():
 	$Contenido/Botones/BtnCreditos.text = "Ruth Rodriguez & Daniel De la Cruz"
 	await get_tree().create_timer(2.0).timeout
-	$Contenido/Botones/BtnCreditos.text= "CREDITOS"
-	
+	$Contenido/Botones/BtnCreditos.text = "CREDITOS"
+
 func _on_bnt_salir_pressed():
 	get_tree().quit()
-	
-func _on_btn_salir_pressed() -> void:
-	pass # Replace with function body.
-	
+
 func _on_btn_tienda_pressed():
 	get_tree().change_scene_to_file("res://scenes/ui/UpgradeShop.tscn")
+
+
+func _on_btn_salir_pressed() -> void:
+	pass # Replace with function body.
