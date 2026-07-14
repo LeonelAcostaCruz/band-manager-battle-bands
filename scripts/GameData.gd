fed919885c1425049d6400333ca50e9ee4e5360a
Leonel <acostaleo18@gmail.com>
2026-07-14 17:28:42 -0600

    feat: agregar enemigos niveles 2-5 con habilidades y GameData con NIVELES

--- scripts/GameData.gd
+++ scripts/GameData.gd
@@ -2,8 +2,16 @@ extends Node
 
 var prestigio: int = 0
 var nivel_actual: int = 1
-var mejoras_compradas: Array = []  # guarda los IDs de mejoras ya compradas
+var mejoras_compradas: Array = []
 var ultimo_resultado_victoria: bool = true
+
+const NIVELES = {
+	1: {"nombre": "Garage",               "enemigo": "res://resources/characters/los_novatos.tres",      "prestigio": 50},
+	2: {"nombre": "Bar Local",            "enemigo": "res://resources/characters/ruido_callejero.tres",  "prestigio": 75},
+	3: {"nombre": "Escuela de Arte",      "enemigo": "res://resources/characters/tinta_negra.tres",      "prestigio": 100},
+	4: {"nombre": "Callejon Underground", "enemigo": "res://resources/characters/soul_breaker.tres",     "prestigio": 150},
+	5: {"nombre": "Club Underground",     "enemigo": "res://resources/characters/metal_excesivo.tres",   "prestigio": 200},
+}
 
 func ganar_prestigio(cantidad: int):
 	prestigio += cantidad
